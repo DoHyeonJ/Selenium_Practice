08e4e8d5e99d00790429b07dadc9842bb92b8b21
dohyeonj <jdh7693@naver.com>
2021-07-04 19:00:13 +0900

    데이터 크롤링

--- selenium_test.py
+++ selenium_test.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
 
-path = "/Users/jodohyeon/workspace/selenium_practice/chromedriver"
-driver = webdriver.Chrome(path)
+# 드라이버 절대 경로 정보
+chromedriver = '/Users/jodohyeon/workspace/selenium_practice/chromedriver'
+# 크롬 드라이버 연동
+driver = webdriver.Chrome(chromedriver)
+# 크롤링할 URL
+driver.get('https://www.alibaba.com/product-detail/Mask-Mask-Factory-Supply-Disposable-Medical_62597405290.html?spm=a2700.galleryofferlist.normal_offer.d_image.1b70135e1CgbXq&s=p')
 
-driver.get("http://python.org")
+print(driver.find_element_by_class_name('module-pdp-title').text, "상품 제목")
+
+for i in driver.find_elements_by_class_name('ma-ladder-price-item'):
+    print(i.text, "상품 가격 조건")
+
+print(driver.find_element_by_class_name('do-entry-separate').text, "상품 상세")
+
